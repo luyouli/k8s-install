c299b1f25de99e1ed11025a1e70faf1445f8fc65
jiang.hc <jiang.hc@founder.com.cn>
2019-05-06 15:16:27 +0800

    xxx

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "安装ansible、git"
+cat <<EOF >  /etc/yum.repos.d/ansible
+[epel]
+name = all source for ansible
+baseurl = https://mirrors.aliyun.com/epel/7/x86_64/
+enabled = 1
+gpgcheck = 0
+[ansible]
+name = all source for ansible
+baseurl = http://mirrors.aliyun.com/centos/7.5.1804/os/x86_64/
+enabled = 1
+gpgcheck = 0
+EOF
+
+yum clean all && yum install -y ansible && yum -y install git
+sed -i 's/#host_key_checking/host_key_checking/g' /etc/ansible/ansible.cfg
+
+echo "git克隆ansible脚本"
+git clone https://github.com/javac2005/k8s-install.git
+
