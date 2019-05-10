351bc149b02f53cef5245dc3ea401fd94a94a8ca
jiang.hc <jiang.hc@founder.com.cn>
2019-05-10 09:55:11 +0800

    xxx

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ EOF
 
 echo "安装ansible和git"
 yum clean all && yum install -y ansible && yum -y install git
+sed -i 's/#host_key_checking/host_key_checking/g' /etc/ansible/ansible.cfg
 
 echo "git clone安装脚本"
 git clone https://github.com/javac2005/k8s-install.git
